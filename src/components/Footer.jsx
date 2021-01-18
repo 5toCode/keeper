345c594f0f04f986480a6d848e29f86e9c892d2c
5toCode <70207997+5toCode@users.noreply.github.com>
2021-01-17 20:30:51 -0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const year = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {year} 5toCode, LLC</p>
+    </footer>
+  );
+}
+
+export default Footer;
